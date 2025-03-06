93e00db0705ffe051e5087ec0a7e0623723a6942
ContardiC <c.contardi@spacecoding.it>
2025-03-06 11:32:04 +0100

    add: structs for Data, Ora, Persona, Appuntamento

--- GestoreAppuntamenti/main.c
+++ GestoreAppuntamenti/main.c
@@ -6,6 +6,27 @@
 //
 
 #include <stdio.h>
+typedef struct{
+    int giorno;
+    int mese;
+    int anno;
+}Data;
+typedef struct{
+    int ore;
+    int minuti;
+    int secondi;
+}Ora;
+typedef struct{
+    char nome[256];
+    char cognome[256];
+    Data data_nascita;
+}Persona;
+typedef struct{
+    Persona persona;
+    Data data_appuntamento;
+    Ora ora_appuntamento;
+    char motivo[256];
+}Appuntamento;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
